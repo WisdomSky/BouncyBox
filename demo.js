53ec1bd089031050ce390cd119dbf17d01c103e7
WisdomSky <julianpaolodayag123@gmail.com>
2014-02-26 01:59:37 -0800

    demo.js

---
+++ demo.js
@@ -0,0 +1,30 @@
+$(function(){
+
+    $("#bouncyBox").bouncyBoxRegister();
+
+
+
+
+    $("#bounce").click(function(){
+    
+        $("#bouncyBox").html("This is a Bouncy Box example!");
+ 
+        $("#bouncyBox").bouncyBox(500,function(){
+            $(this).html("Click this box to dismiss...");
+        });
+        
+    });
+
+
+
+
+
+    $("#bouncyBox").click(function(){
+
+        $(this).bouncyBoxDismiss(1000);
+        
+    });
+
+
+
+});
